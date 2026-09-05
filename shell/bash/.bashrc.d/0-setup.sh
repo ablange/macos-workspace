93e7dc6c1212a3304abca3cb9f2f9b6a589a6ae1
Andrew Lange <43223360+ablange@users.noreply.github.com>
2026-09-05 09:10:21 -0400

    feature/pyenv (#5)

--- shell/bash/.bashrc.d/0-setup.sh
+++ shell/bash/.bashrc.d/0-setup.sh
@@ -1,3 +1,9 @@
 # shell/bash/.bashrc.d/0-setup.sh - Bash aliases and global settings
 
 alias ll='ls -a -A -l'
+
+# pipx installs apps into ~/.local/bin (replaces `pipx ensurepath`).
+case ":$PATH:" in
+    *":$HOME/.local/bin:"*) ;;
+    *) export PATH="$HOME/.local/bin:$PATH" ;;
+esac
